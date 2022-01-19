1ee3120423cb0599c54ac9c2ae17275832a99350
Max Liu <69723902+Snow-0@users.noreply.github.com>
2022-01-19 16:15:23 -0500

    Add files via upload

--- flappy_bird/main.py
+++ flappy_bird/main.py
@@ -1,63 +1,66 @@
 import pygame
+from pygame.locals import *
 import os
-from bird import Bird
-
-WIDTH, HEIGHT = 500, 900
 
 
-pygame.display.init()
-
-WIN = pygame.display.set_mode(size=(WIDTH, HEIGHT))
-
+clock = pygame.time.Clock()
 FPS = 60
 
-PATH = 'flappy_bird/assets/sprites/'
+pygame.init()
 
-BG = pygame.transform.scale(pygame.image.load(os.path.join(
-    PATH, 'background-day.png')), (WIDTH, HEIGHT))
-BASE = pygame.transform.scale(pygame.image.load(
-    os.path.join(PATH, 'base.png')), (900, 300))
+SCREEN_WIDTH = 864
+SCREEN_HEIGHT = 936
 
-bird = Bird(100, 450)
-bird.pos_x = 10
-bird.pos_y = 10
-
-sprites = pygame.sprite.Group()
-sprites.add(bird)
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Flappy Bird")
 
 
-def draw():
-    WIN.blit(BG, (0, 0))
-    WIN.blit(BASE, (0, 750))
+# Game Variables
+ground_scroll = 0
+scroll_speed = 4
 
-    sprites.draw(WIN)
-    sprites.update()
+# load images
+bg = pygame.image.load(os.path.join("flappy_bird/img", "bg.png"))
+base = pygame.image.load(os.path.join("flappy_bird/img", "ground.png"))
+# Scale the image to window size
+
+# creates the bird by inheriting pygame.sprite.Sprite
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("flappy_bird/img/bird1.png")
+        # creates a rectangle from the boundmaries of the image
+        self.rect = self.image.get_rect()
+        # centers the rectangle
+        self.rect.center = [x, y]
+
+
+bird_group = pygame.sprite.Group()
+# positions flappy on the screen
+flappy = Bird(100, int(SCREEN_HEIGHT / 2))
+
+bird_group.add(flappy)
+
+
+run = True
+while run:
+
+    clock.tick(FPS)
+    screen.blit(bg, (0, 0))
+
+    bird_group.draw(screen)
+
+    # draw and scroll
+    screen.blit(base, (ground_scroll, 768))
+    ground_scroll -= scroll_speed
+    if abs(ground_scroll) > 35:
+        ground_scroll = 0
+    # Must be included
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
     pygame.display.update()
 
 
-def main():
-    run = True
-
-    # main loop
-    while run:
-
-        clock = pygame.time.Clock()
-        clock.tick(FPS)
-        key_pressed = pygame.key.get_pressed()
-
-        for event in pygame.event.get():
-
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
-
-            if key_pressed[pygame.K_SPACE]:
-                bird.move(10)
-                bird.animate()
-
-        draw()
-
-
-if __name__ == '__main__':
-    main()
+pygame.quit()
