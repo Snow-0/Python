f3687582fe10634a813e8b4de3fe32b7b3b1dac6
Max <max@max.com>
2022-05-13 20:01:06 -0400

    added profile pictures and editing feature

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import (StringField, PasswordField, BooleanField, SubmitField,
+                     TextAreaField)
+from wtforms.validators import (ValidationError, DataRequired, Email, EqualTo,
+                                Length)
 from app.models import User
 
 
@@ -13,7 +15,7 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
-    email = StringField("Username", validators=[DataRequired(), Email()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     password_two = PasswordField(
         "Repeat Password", validators=[DataRequired(), EqualTo("password")]
@@ -29,3 +31,8 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError("Please use a different email address.")
+
+class EditProfileForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
+    submit = SubmitField("Submit")
