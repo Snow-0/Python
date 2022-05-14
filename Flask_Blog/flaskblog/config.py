01c777ada355b76fc806ec8635dc704a563afdea
Max <max@max.com>
2022-05-13 20:00:53 -0400

    deleted flask_blog

--- Flask_Blog/flaskblog/config.py
+++
@@ -1,9 +0,0 @@
-class Config:
-    SECRET_KEY = "468b9b112e9901ca981d4629c1c733ee"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
-    MAIL_SERVER = "smtp.mailtrap.io"
-    MAIL_PORT = 2525
-    MAIL_USERNAME = "5c2afa86b0f33e"
-    MAIL_PASSWORD = "9acbc27b2ae9da"
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
